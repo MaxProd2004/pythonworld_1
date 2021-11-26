a4826e30d684fd2377fe3c3f53ae0af3703e294d
MaxProd2004 <87067987+MaxProd2004@users.noreply.github.com>
2021-11-27 00:36:57 +0200

    Add files via upload

---
+++ Задание 1.py
@@ -0,0 +1,11 @@
+def arithmetic(num_1, num_2, arop):
+    if arop == '+':
+        print(num_1 + num_2)
+    if arop == '-':
+        print(num_1 - num_2)
+    if arop == '*':
+        print(num_1 * num_2)
+    if arop == '/':
+        print(num_1 / num_2)
+        
+arithmetic(34, 45, '*')
